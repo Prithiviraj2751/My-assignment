4cee40865155e2e0363c9f78ac4841192c6a1122
Prithiviraj2751 <prithiviraj2751@gmail.com>
2025-09-07 20:51:40 +0530

    Add files via upload
    week 1 Day2

---
+++ assignment/CheckNumberIspositive.java
@@ -0,0 +1,15 @@
+package week1.assignment;
+
+public class CheckNumberIspositive {
+
+	public static void main(String[] args) {
+		int a = -1;
+		if(a>0) {
+			System.out.println("Number is positive");
+		} else if (a<0) {
+			System.out.println("Number is negative");
+		}
+
+	}
+
+}
---
+++ assignment/Chrome.java
@@ -0,0 +1,19 @@
+package week1.assignment;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean Beta=false;
+		int Releaseyear = 2008;
+		char shortcutkey= 'c';
+		System.out.println("developer="+developer);
+		System.out.println("version="+version);
+		System.out.println("Released year="+Releaseyear);
+		System.out.println("alpha verion="+Beta);
+		System.out.println("shortcut key="+shortcutkey);
+		
+	}
+
+}

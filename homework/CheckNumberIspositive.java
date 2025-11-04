6804f9ac15ca055f43ef7a92e29ec783b0bde13b
Prithiviraj2751 <prithiviraj2751@gmail.com>
2025-11-04 14:49:58 +0530

    Add files via upload

---
+++ homework/CheckNumberIspositive.java
@@ -0,0 +1,15 @@
+package week1.assignment;
+
+public class CheckNumberIspositive {
+
+	public static void main(String[] args) {
+		int a = 10;
+		if(a>0) {
+			System.out.println("Number is positive");
+		} else if (a<0) {
+			System.out.println("Number is negative");
+		}
+
+	}
+
+}
